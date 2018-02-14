2a851db69b7fcfcc36528b1b4ebda8d6ef41bc17
Lukas Frei <lukas.frei@zuehlke.cim>
2018-02-14 14:49:33 +0100

    Add config for service endpoints

--- src/main/java/com/zuehlke/MovieTicketServiceApplication.java
+++ src/main/java/com/zuehlke/MovieTicketServiceApplication.java
@@ -1,5 +1,6 @@
 package com.zuehlke;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,17 +8,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MovieTicketServiceApplication {
 
+	@Value("${endpoint.movie-service}")
+	private String movieServiceEndpoint;
+
+	@Value("${endpoint.movie-rating-service}")
+	private String ratingServiceEndpoint;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MovieTicketServiceApplication.class, args);
 	}
 
 	@Bean
 	MovieServiceAdapter createMovieServiceAdapter() {
-		return new MovieServiceAdapter("https://movie-service.herokuapp.com");
+		return new MovieServiceAdapter(movieServiceEndpoint);
 	}
 
 	@Bean
 	RatingServiceAdapter createRatingServiceAdapter() {
-		return new RatingServiceAdapter("https://movie-rating-service.herokuapp.com");
+		return new RatingServiceAdapter(ratingServiceEndpoint);
 	}
 }
